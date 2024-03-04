1e18a50701d036fb87dc0508df295a24e5b6f3fd
“[shripad <shripad0618@gmail.com>
2024-03-04 07:36:54 +0530

    Data ingestion completed

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 
 conda create -n venv python=3.8 -y
 ```
+
+update config.yaml
+update schema.yaml
+update params.yaml
+update the entity
+update the configuration manager in src config
+update the components
+update the pipeline
+update the main.py
+update the app.py
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from ML_project import logger
+from ML_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+#from ML_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+#from ML_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+#from ML_project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+#from ML_project.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
-logger.info("THis is our custom log v2")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
